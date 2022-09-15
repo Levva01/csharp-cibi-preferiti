f14f80707f451ed0dfa2c085e62f016669cbe4e1
Levva01 <vellanifederico13@gmail.com>
2022-09-15 15:34:06 +0200

    classifica in ordine

--- Program.cs
+++ Program.cs
@@ -10,9 +10,22 @@ string[] favouriteFoods = new string[] {
 
 int i = 0;
 
-for (i = 0; i < favouriteFoods.Length - 1; i++)
+for (i = 1; i < favouriteFoods.Length-1; i++)
 {
     i++;
 }
 
 Console.WriteLine($"Lunghezza della classifica {i}");
+
+favouriteFoods[1] = "Gelato";
+favouriteFoods[2] = "Torta";
+favouriteFoods[3] = "Pasta";
+favouriteFoods[4] = "Sushi";
+
+Console.WriteLine("");
+Console.WriteLine("Classifica: ");
+
+for(i = 0; i < favouriteFoods.Length; i++)
+{
+    Console.WriteLine($"{i+1}. {favouriteFoods[i]}");
+}

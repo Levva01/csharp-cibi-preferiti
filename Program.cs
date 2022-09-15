18d6678b1d67ff2fc94d4dda60f4de5d6c7aad25
Levva01 <vellanifederico13@gmail.com>
2022-09-15 15:22:50 +0200

    Lunghezza classifica stampata

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 string[] favouriteFoods = new string[] {
                 "Pizza",
@@ -8,3 +7,12 @@ string[] favouriteFoods = new string[] {
                 "Torta",
                 "Pasta"
 };
+
+int i = 0;
+
+for (i = 0; i < favouriteFoods.Length - 1; i++)
+{
+    i++;
+}
+
+Console.WriteLine($"Lunghezza della classifica {i}");
